8988b618671b38050e927bed69c6ed3d900cf5f3
deniljohn27 <117897719+deniljohn27@users.noreply.github.com>
2023-05-21 12:37:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function highlight() {
     //Write your code here
+	let data=document.getElemetsByTagName("strong");
+	for(let i=0;i<data.length;i++){
+		data[i].className="strong_color"
+	}
 
 
 }
